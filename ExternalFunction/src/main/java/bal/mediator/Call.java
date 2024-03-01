b7e46ec6d590e40cd92921419ff41a8d4eb7fe37
samithkavishke <samithkarunathilake@email.com>
2024-03-01 11:07:28 +0530

    now this is working application with ExternalFunction-1.0.0-SNAPSHOT.jar

--- ExternalFunction/src/main/java/bal/mediator/Call.java
+++ ExternalFunction/src/main/java/bal/mediator/Call.java
@@ -1,59 +1,67 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package bal.mediator;
 
+import io.ballerina.runtime.api.values.BXml;
 import io.ballerina.runtime.api.Environment;
 import io.ballerina.runtime.api.Future;
 import io.ballerina.runtime.api.PredefinedTypes;
 import io.ballerina.runtime.api.async.Callback;
+import io.ballerina.runtime.api.async.StrandMetadata;
 import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.internal.BalRuntime;
 import io.ballerina.runtime.internal.scheduling.Scheduler;
 import io.ballerina.runtime.internal.scheduling.Strand;
 import io.ballerina.runtime.internal.values.BmpStringValue;
+import org.apache.synapse.core.axis2.Axis2MessageContext;
+
+import java.util.Map;
 
 public class Call {
+	public Call() {
+	}
+
 	public static void call(Environment env, BObject object) {
 		final Strand strand = Scheduler.getStrand();
 		Scheduler scheduler = strand.scheduler;
 		BalRuntime balRuntime = new BalRuntime(scheduler);
-
-		final String payload = (String) strand.getProperty("payload");
-        String firstArgument = strand.getProperty("firstArgument").toString();
-		String secondArgument = strand.getProperty("secondArgument").toString();
+		Axis2MessageContext messageContext = (Axis2MessageContext) strand.getProperty("messageContext");
+		Object firstArgument = strand.getProperty("firstArgument").toString();
+		Object secondArgument = strand.getProperty("secondArgument").toString();
+		String thirdArgument = strand.getProperty("thirdArgument").toString();
+		String fourthArgument = strand.getProperty("fourthArgument").toString();
+		String fifthArgument = strand.getProperty("fifthArgument").toString();
+		String sixthArgument = strand.getProperty("sixthArgument").toString();
 		String functionName = strand.getProperty("functionName").toString();
 
-		Object[] argumentsList = buildArguments(payload, secondArgument);
-
+		Object[] argumentsList = buildArguments(firstArgument, secondArgument);
 		final Future balFuture = env.markAsync();
 		Callback returnCallback = new Callback() {
-			@Override
 			public void notifySuccess(Object result) {
-				// Note: Here set the result for the payload.
 				strand.setProperty("result", result.toString());
 				balFuture.complete(result);
 			}
 
-			@Override
 			public void notifyFailure(BError result) {
-                strand.setProperty("result", result.toString());
+				strand.setProperty("result", result.toString());
 				balFuture.complete(result);
 			}
 		};
-
-		balRuntime.invokeMethodAsyncSequentially(object, functionName, null,
-				null, returnCallback, null,
-				PredefinedTypes.TYPE_ANY, argumentsList);
+		balRuntime.invokeMethodAsyncSequentially(object, functionName, (String)null, (StrandMetadata)null, returnCallback, (Map)null, PredefinedTypes.TYPE_ANY, argumentsList);
 	}
 
-	private static Object[] buildArguments(String... arguments) {
-		// Note: This function constructs the arguments array for the Ballerina function invocation.
-		// Depending on the function signature on the Ballerina side, you may need to adjust the number
-		// of arguments and their types accordingly.
+	private static Object[] buildArguments(Object... arguments) {
 		Object[] args = new Object[arguments.length * 2];
+
 		for (int i = 0; i < arguments.length; i++) {
-			args[i * 2] = new BmpStringValue(arguments[i]);
+			args[i * 2] = new BmpStringValue(arguments[i].toString()) ;
 			args[i * 2 + 1] = true;
 		}
+
 		return args;
 	}
 }
